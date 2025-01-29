ce7aa00636b1fad8fcf388e09db659ab73c77654
bbbang105 <2018111366@dgu.ac.kr>
2025-01-29 15:46:49 +0900

    #14 [remove] : UserResolver를 제거한다

--- src/main/java/server/poptato/global/config/WebConfig.java
+++ src/main/java/server/poptato/global/config/WebConfig.java
@@ -3,18 +3,12 @@ package server.poptato.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import server.poptato.user.resolver.UserResolver;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-    private final UserResolver userResolver;
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
@@ -24,11 +18,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true)
                 .maxAge(3000);
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(userResolver);
-
     }
 }
