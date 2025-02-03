1f40d9cfcd2f30ef04cafde96a8bcc3f4a2cd51c
bbbang105 <2018111366@dgu.ac.kr>
2025-02-04 01:51:57 +0900

    #17 [feat] : 액세스 & 리프레쉬 토큰 에러 응답을 구분한다

--- src/main/java/server/poptato/auth/status/AuthErrorStatus.java
+++ src/main/java/server/poptato/auth/status/AuthErrorStatus.java
@@ -9,13 +9,15 @@ import server.poptato.global.response.dto.ErrorReasonDto;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorStatus implements BaseErrorCode {
-    _TOKEN_NOT_EXIST(HttpStatus.UNAUTHORIZED, "AUTH-001", "토큰이 존재하지 않습니다."),
-    _TOKEN_TIME_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH-002", "토큰이 만료되었습니다."),
-    _INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-003", "토큰이 유효하지 않습니다."),
+    _NOT_EXIST_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-001", "액세스 토큰이 존재하지 않습니다."),
+    _EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-002", "액세스 토큰이 만료되었습니다."),
+    _INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-003", "액세스 토큰이 유효하지 않습니다."),
     _DIFFERENT_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-004", "레디스에 저장된 리프레쉬 토큰과 다릅니다."),
     _NOT_FOUND_VALID_PUBLIC_KEY(HttpStatus.UNAUTHORIZED, "AUTH-005", "유효한 공개 키를 찾을 수 없습니다."),
     _PUBLIC_KEY_GENERATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH-006", "공개 키 생성에 실패했습니다."),
     _INVALID_USER_ID_IN_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-007", "토큰 내 USER_ID가 유효하지 않습니다."),
+    _EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-008", "리프레쉬 토큰이 만료되었습니다."),
+    _INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH-009", "리프레쉬 토큰이 유효하지 않습니다."),
     ;
 
     private final HttpStatus httpStatus;
