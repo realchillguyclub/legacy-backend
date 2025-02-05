12c9df3e8ff9fb7bf878a7fe22ec57b67b502f21
bbbang105 <2018111366@dgu.ac.kr>
2025-02-05 13:40:40 +0900

    #21 [fix] : 마감 기한의 null을 허용하여 문제를 해결한다

--- src/main/java/server/poptato/todo/api/request/DeadlineUpdateRequestDto.java
+++ src/main/java/server/poptato/todo/api/request/DeadlineUpdateRequestDto.java
@@ -1,12 +1,10 @@
 package server.poptato.todo.api.request;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Future;
 
 import java.time.LocalDate;
 
 public record DeadlineUpdateRequestDto (
-        @NotNull(message = "마감일은 필수입니다.")
         @Future(message = "마감일은 미래 날짜여야 합니다.")
         LocalDate deadline
 ){
