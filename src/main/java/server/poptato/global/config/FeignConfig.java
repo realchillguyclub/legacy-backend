c38b6257ca928b10abbc05443f83350c6ac63328
bbbang105 <2018111366@dgu.ac.kr>
2025-02-11 04:25:38 +0900

    #3 [chore] : 베이스 패키지 경로를 수정한다

--- src/main/java/server/poptato/global/config/FeignConfig.java
+++ src/main/java/server/poptato/global/config/FeignConfig.java
@@ -4,6 +4,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(basePackages = "server.poptato.external.oauth.kakao")
+@EnableFeignClients(basePackages = "server.poptato.external.oauth")
 public class FeignConfig {
 }
