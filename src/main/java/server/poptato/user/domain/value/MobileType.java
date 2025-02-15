72542a6a30613bc7340127f0dd368306aff6b597
woong2e <tlswodnd12345@naver.com>
2025-02-15 23:19:58 +0900

    [feat] : 모바일 타입별 imageUrl 확장자를 설정한다  (#37)

--- src/main/java/server/poptato/user/domain/value/MobileType.java
+++ src/main/java/server/poptato/user/domain/value/MobileType.java
@@ -1,5 +1,16 @@
 package server.poptato.user.domain.value;
 
+import lombok.Getter;
+
+@Getter
 public enum MobileType {
-    ANDROID, IOS
+    ANDROID(".sgv"),
+    IOS(".pdf");
+
+    private final String imageUrlExtension;
+
+    MobileType(String imageUrlExtension) {
+        this.imageUrlExtension = imageUrlExtension;
+    }
+
 }
