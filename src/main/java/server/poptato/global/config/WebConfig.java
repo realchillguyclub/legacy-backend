48ba53aceb281ffd8e194e385afa236e3ae965e6
bbbang105 <2018111366@dgu.ac.kr>
2025-02-17 21:11:50 +0900

    #41 [fix] : 모든 메서드의 요청을 허용하여 CORS 문제를 해결한다

--- src/main/java/server/poptato/global/config/WebConfig.java
+++ src/main/java/server/poptato/global/config/WebConfig.java
@@ -2,7 +2,6 @@ package server.poptato.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name())
+                .allowedMethods("*")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true)
                 .maxAge(3000);
