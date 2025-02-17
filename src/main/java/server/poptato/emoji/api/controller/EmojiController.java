d79a9b3fca8e123b3dc321fa694469b8fbcbba55
신재웅_Shin JaeWoong <104746510+woong2e@users.noreply.github.com>
2025-02-17 22:28:36 +0900

    [fix] : IOS 아이콘 이미지가 깨지지 않도록 수정한다 (#39)
    * [chore] : application.yaml 파일을 추가한다 (#37)
    
    * [feat] : 이모지 imageUrl에 대한 요청시 클라이언트의 운영체제를 파라미터로 받는다 (#37)
    
    * [feat] : 이모지 imageUrl에 대한 요청시 클라이언트의 운영체제에 따라 url의 확장자를 바꾼다 (#37)
    
    * [test] : 파라미터에 맞춰 테스트를 수정한다 (#37)
    
    * [feat] : 모바일 타입별 imageUrl 확장자를 설정한다  (#37)
    
    * [feat] : 모바일 타입 string을 MobileType으로 변경해주는 컨버터를 추가 및 예외처리를 진행한다  (#37)
    
    * [style] : sgv -> svg 오타를 수정한다  (#37)
    
    * [refactor] : @RequestParam에 value 속성을 추가한다 (#37)
    
    * [style] : 기본 확장자 오타 수정 및 상수화를 한다 (#37)
    
    * [refactor] : 파일 확장자 변환에 관한 중복 코드를 FileUtil로 분리한다 (#37)
    
    * [refactor] : @RequiredArgsConstructor 애노테이션을 추가한다 (#37)
    
    * [feat] : 모든 HTTP 메서드를 허용하도록 변경한다 (#37)

--- src/main/java/server/poptato/emoji/api/controller/EmojiController.java
+++ src/main/java/server/poptato/emoji/api/controller/EmojiController.java
@@ -2,14 +2,12 @@ package server.poptato.emoji.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import server.poptato.emoji.application.response.EmojiResponseDto;
 import server.poptato.emoji.application.service.EmojiService;
 import server.poptato.global.response.ApiResponse;
 import server.poptato.global.response.status.SuccessStatus;
+import server.poptato.user.domain.value.MobileType;
 
 @RestController
 @RequestMapping("/emojis")
@@ -23,16 +21,18 @@ public class EmojiController {
      *
      * 사용 가능한 이모지 목록을 페이지네이션 형식으로 조회합니다.
      *
+     * @param mobileType 클라이언트 운영체제
      * @param page 요청 페이지 번호 (기본값: 0)
      * @param size 한 페이지당 항목 수 (기본값: 70)
      * @return 그룹화된 이모지 목록과 페이징 정보를 포함한 응답
      */
     @GetMapping
     public ResponseEntity<ApiResponse<EmojiResponseDto>> getCategories(
+            @RequestParam(value = "mobileType", defaultValue = "ANDROID") MobileType mobileType,
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "70") int size
     ) {
-        EmojiResponseDto response = emojiService.getGroupedEmojis(page, size);
+        EmojiResponseDto response = emojiService.getGroupedEmojis(mobileType, page, size);
         return ApiResponse.onSuccess(SuccessStatus._OK, response);
     }
 }
