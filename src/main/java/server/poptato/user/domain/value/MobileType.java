04642ad3b0fe012e9fa4fff731ae9003b1081058
woong2e <tlswodnd12345@naver.com>
2025-02-17 20:21:01 +0900

    [refactor] : @RequiredArgsConstructor 애노테이션을 추가한다 (#37)

--- src/main/java/server/poptato/user/domain/value/MobileType.java
+++ src/main/java/server/poptato/user/domain/value/MobileType.java
@@ -1,16 +1,13 @@
 package server.poptato.user.domain.value;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum MobileType {
     ANDROID(".svg"),
     IOS(".pdf");
 
     private final String imageUrlExtension;
-
-    MobileType(String imageUrlExtension) {
-        this.imageUrlExtension = imageUrlExtension;
-    }
-
 }
