09293906d13a33a72688b081870c762c94d56ef2
woong2e <tlswodnd12345@naver.com>
2025-02-19 15:06:11 +0900

    [chore] : 주석을 추가한다 (#43)

--- src/main/java/server/poptato/external/firebase/service/FCMService.java
+++ src/main/java/server/poptato/external/firebase/service/FCMService.java
@@ -9,6 +9,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class FCMService {
 
+    /**
+     * 푸시 알림을 보내는 메서드.
+     * firebase에 푸시 알림 요청을 보낸다. 예외 발생 시 호출하는 메서드로 FirebaseMessagingException 예외를 던진다
+     * @param userToken
+     * @param title
+     * @param message
+     * @throws FirebaseMessagingException
+     */
     public void sendPushNotification(String userToken, String title, String message) throws FirebaseMessagingException {
         Message firebaseMessage = Message.builder()
                 .setToken(userToken)
