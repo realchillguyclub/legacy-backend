16bfe9960275de242506e660b618f0c9cc1045f5
bbbang105 <2018111366@dgu.ac.kr>
2025-02-26 15:50:42 +0900

    #50 [feat] : 불필요한 '어제' 상태 변경 메서드를 제거하고, FCM 토큰 관련 기능을 분리한다

--- src/main/java/server/poptato/todo/application/TodoScheduler.java
+++ src/main/java/server/poptato/todo/application/TodoScheduler.java
@@ -1,27 +1,21 @@
 package server.poptato.todo.application;
 
-import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.MessagingErrorCode;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import server.poptato.external.firebase.service.FCMService;
 import server.poptato.todo.domain.entity.Todo;
 import server.poptato.todo.domain.repository.TodoRepository;
 import server.poptato.todo.domain.value.TodayStatus;
 import server.poptato.todo.domain.value.Type;
-import server.poptato.user.domain.entity.Mobile;
-import server.poptato.user.domain.entity.User;
-import server.poptato.user.domain.repository.MobileRepository;
 import server.poptato.user.domain.repository.UserRepository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,234 +23,97 @@ import java.util.stream.Collectors;
 public class TodoScheduler {
     private final TodoRepository todoRepository;
     private final UserRepository userRepository;
-    private final MobileRepository mobileRepository;
-    private final FCMService fcmService;
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final TodoService todoService;
 
     /**
-     * 매일 자정에 할 일의 상태(Type)를 업데이트하고 마감 알림을 전송합니다.
+     * 매일 새벽 특정 시간에 할 일 상태를 업데이트한다.
      */
-    @Scheduled(cron = "0 0 0 * * *")
-    @Transactional
+    @Scheduled(cron = "${scheduling.todoCron}")
     public void updateTodoType() {
-        List<Long> updatedTodoIds = new ArrayList<>();
-        updateTodo(updatedTodoIds);
-        sendDeadlineNotifications();
-        deleteOldFcmTokens();
+        updateTodayTodosAndSave();
+        updateDeadlineTodoAsync();
     }
 
     /**
-     * 할 일의 상태(Type)를 업데이트합니다.
-     *
-     * @param updatedTodoIds 업데이트된 할 일의 ID 목록
+     * 할 일의 상태(Type)를 업데이트하고 저장한다.
      */
-    @Async
-    public void updateTodo(List<Long> updatedTodoIds) {
-        Map<Long, Integer> userIdToStartingOrder = new HashMap<>();
-        Map<Long, List<Todo>> userIdAndTodaysMap = updateTodays(updatedTodoIds, userIdToStartingOrder);
-        List<Todo> yesterdayTodos = updateYesterdays(updatedTodoIds, userIdToStartingOrder);
-        save(userIdAndTodaysMap, yesterdayTodos);
-        updateDeadlineTodo();
+    @Transactional
+    public void updateTodayTodosAndSave() {
+        Map<Long, List<Todo>> userIdAndTodaysMap = updateTodayTodos();
+        saveUpdatedTodos(userIdAndTodaysMap);
     }
 
     /**
-     * 오늘(TODAY) 상태의 할 일을 업데이트합니다.
+     * 오늘(TODAY) 상태의 할 일 중 완료 여부에 따라 상태를 변경한다.
+     * 완료된 반복 할 일은 백로그로 이동하며, 미완료된 할 일은 어제로 변경된다.
      *
-     * @param updatedTodoIds 업데이트된 할 일의 ID 목록
-     * @param userIdToStartingOrder 사용자 ID별 시작 순서 맵
      * @return 사용자 ID별 오늘의 할 일 목록
      */
-    private Map<Long, List<Todo>> updateTodays(List<Long> updatedTodoIds, Map<Long, Integer> userIdToStartingOrder) {
-        Map<Long, List<Todo>> userIdAndTodaysMap = todoRepository.findByType(Type.TODAY)
+    private Map<Long, List<Todo>> updateTodayTodos() {
+        Map<Long, Integer> userIdToStartingOrder = new HashMap<>();
+        Map<Long, List<Todo>> userIdAndTodayTodosMap = todoRepository.findByType(Type.TODAY)
                 .stream()
                 .collect(Collectors.groupingBy(Todo::getUserId));
 
-        userIdAndTodaysMap.forEach((userId, todos) -> {
-            userIdToStartingOrder.putIfAbsent(userId, todoRepository.findMaxBacklogOrderByUserIdOrZero(userId) + 1);
-            int startingOrder = userIdToStartingOrder.get(userId);
+        userIdAndTodayTodosMap.forEach((userId, todos) -> {
+            int startingOrder = userIdToStartingOrder.computeIfAbsent(
+                    userId, id -> todoRepository.findMaxBacklogOrderByUserIdOrZero(id) + 1
+            );
 
             for (Todo todo : todos) {
                 if (todo.getTodayStatus() == TodayStatus.COMPLETED && todo.isRepeat()) {
+                    // 완료한 반복 할 일이라면 백로그로 이동
                     todo.setType(Type.BACKLOG);
                     todo.setTodayStatus(null);
                     todo.setTodayOrder(null);
                     todo.setBacklogOrder(startingOrder++);
-                    updatedTodoIds.add(todo.getId());
-                    continue;
-                }
-
-                if (todo.getTodayStatus() == TodayStatus.INCOMPLETE) {
+                } else if (todo.getTodayStatus() == TodayStatus.INCOMPLETE) {
+                    // 미완료라면 모두 '어제'로 이동
                     todo.setType(Type.YESTERDAY);
                     todo.setTodayOrder(null);
                     todo.setBacklogOrder(startingOrder++);
-                    updatedTodoIds.add(todo.getId());
                 }
             }
-
-            userIdToStartingOrder.put(userId, startingOrder);
         });
-        return userIdAndTodaysMap;
+
+        return userIdAndTodayTodosMap;
     }
 
     /**
-     * 어제(YESTERDAY) 상태의 할 일을 업데이트합니다.
-     *
-     * @param updatedTodoIds 업데이트된 할 일의 ID 목록
-     * @param userIdToStartingOrder 사용자 ID별 시작 순서 맵
-     * @return 업데이트된 어제의 할 일 목록
-     */
-    private List<Todo> updateYesterdays(List<Long> updatedTodoIds, Map<Long, Integer> userIdToStartingOrder) {
-        return todoRepository.findByType(Type.YESTERDAY)
-                .stream()
-                /* 25.02.20 : 미사용으로 인한 주석 처리
-                .filter(todo -> !updatedTodoIds.contains(todo.getId()))
-                */
-                .peek(todo -> {
-                    Long userId = todo.getUserId();
-
-                    userIdToStartingOrder.putIfAbsent(userId, todoRepository.findMaxBacklogOrderByUserIdOrZero(userId) + 1);
-                    int startingOrder = userIdToStartingOrder.get(userId);
-
-                    if (todo.getTodayStatus() == TodayStatus.INCOMPLETE) {
-                        todo.setType(Type.BACKLOG);
-                        todo.setTodayStatus(null);
-                        todo.setBacklogOrder(startingOrder++);
-                    } else if (todo.getTodayStatus() == TodayStatus.COMPLETED && todo.isRepeat()) {
-                        todo.setType(Type.BACKLOG);
-                        todo.setTodayStatus(null);
-                        todo.setBacklogOrder(startingOrder++);
-                    }
-
-                    userIdToStartingOrder.put(userId, startingOrder);
-                })
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * 업데이트된 할 일을 저장합니다.
+     * 업데이트된 할 일을 한 번에 저장한다.
      *
      * @param userIdAndTodaysMap 사용자 ID별 오늘의 할 일 목록
-     * @param yesterdayTodos 업데이트된 어제의 할 일 목록
      */
-    private void save(Map<Long, List<Todo>> userIdAndTodaysMap, List<Todo> yesterdayTodos) {
-        for (Todo todo : userIdAndTodaysMap.values().stream().flatMap(List::stream).toList()) {
-            todoRepository.save(todo);
-        }
-        for (Todo todo : yesterdayTodos) {
-            todoRepository.save(todo);
-        }
+    private void saveUpdatedTodos(Map<Long, List<Todo>> userIdAndTodaysMap) {
+        userIdAndTodaysMap.values().stream()
+                .flatMap(List::stream)
+                .forEach(todoRepository::save);
     }
 
     /**
-     * 마감기한 날짜가 된 할 일을 오늘 할 일로 옮깁니다.
-     *
-     */
-    private void updateDeadlineTodo() {
-        List<Long> userIds = userRepository.findAllUserIds();
-        updateBacklogTodosToTodayWithBatch(userIds);
-    }
-
-    /**
-     * 백로그를 오늘 할 일로 옮기는 배치작업을 진행합니다.
-     * @param userIds 전체 사용자 ID
-     *
-     */
-    @Transactional
-    protected void updateBacklogTodosToTodayWithBatch(List<Long> userIds) {
-        LocalDate today = LocalDate.now();
-        int batchSize = 50;
-        List<List<Long>> userBatches = splitListIntoBatches(userIds, batchSize);
-        for (List<Long> batch : userBatches) {
-            todoRepository.updateBacklogTodosToToday(today, batch, 0);
-            entityManager.flush();
-            entityManager.clear();
-        }
-    }
-
-    /**
-     * 유저 아이디 리스트를 배치 사이즈로 나누는 작업을 합니다.
-     * @param userIds 전체 사용자 ID
-     * @param batchSize 배치사이즈
-     */
-    private List<List<Long>> splitListIntoBatches(List<Long> userIds, int batchSize) {
-        List<List<Long>> partitions = new ArrayList<>();
-        for (int i = 0; i < userIds.size(); i += batchSize) {
-            partitions.add(userIds.subList(i, Math.min(i + batchSize, userIds.size())));
-        }
-        return partitions;
-    }
-
-    /**
-     * 마감 알림을 전송합니다.
+     * 마감기한이 된 할 일을 오늘 할 일(TODAY)로 변경한다.
      */
     @Async
-    public void sendDeadlineNotifications() {
-        List<User> users = userRepository.findAll();
+    public void updateDeadlineTodoAsync() {
+        LocalDate today = LocalDate.now();
+        int batchSize = 50;
 
-        for (User user : users) {
-            if (Boolean.TRUE.equals(user.getIsPushAlarm())) {
-                List<Todo> todosDueToday = todoRepository.findTodosDueToday(user.getId(), LocalDate.now());
-                sendFcmMessage(user, todosDueToday);
-            }
-        }
+        List<Long> userIds = userRepository.findAllUserIds();
+        splitListIntoBatches(userIds, batchSize).forEach(batch -> {
+            todoService.processUpdateDeadlineTodos(today, batch);
+        });
     }
 
     /**
-     * FCM 메시지를 전송합니다.
+     * 유저 ID 리스트를 배치 크기 단위로 나눈다.
      *
-     * @param user 알림을 받을 사용자
-     * @param todosDueToday 오늘 마감 예정인 할 일 목록
+     * @param userIds   전체 사용자 ID 목록
+     * @param batchSize 배치 크기
+     * @return 배치된 유저 ID 목록
      */
-    private void sendFcmMessage(User user, List<Todo> todosDueToday) {
-        if (!todosDueToday.isEmpty()) {
-            List<Mobile> mobiles = mobileRepository.findAllByUserId(user.getId());
-            for (Mobile mobile : mobiles) {
-                for (Todo todo : todosDueToday) {
-                    String todoContent = formatTodoContent(todo);
-                    sendPushNotificationOrDeleteFcmToken(mobile.getClientId(), todoContent);
-                }
-            }
-        }
-    }
-
-    private void sendPushNotificationOrDeleteFcmToken(String clientId, String todoContent) {
-        try {
-            fcmService.sendPushNotification(
-                    clientId,
-                    "오늘 마감 예정인 할 일",
-                    todoContent
-            );
-        } catch (FirebaseMessagingException e) {
-            if (e.getMessagingErrorCode().equals(MessagingErrorCode.INVALID_ARGUMENT)) {
-                // 토큰이 유효하지 않은 경우
-                mobileRepository.deleteByClientId(clientId);
-            } else if (e.getMessagingErrorCode().equals(MessagingErrorCode.UNREGISTERED)) {
-                // 재발급된 이전 토큰인 경우
-                mobileRepository.deleteByClientId(clientId);
-            }
-            else {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    /**
-     * 비활성 fcm토큰을 삭제하는 메서드.
-     * timestamp를 갱신한지 한달이 지난 fmc토큰을 삭제한다.
-     */
-    private void deleteOldFcmTokens() {
-        LocalDateTime oneMonthAgo = LocalDateTime.now().minusMonths(1);
-        mobileRepository.deleteOldTokens(oneMonthAgo);
-    }
-
-    /**
-     * 할 일 내용을 포맷합니다.
-     *
-     * @param todo 할 일 객체
-     * @return 포맷된 할 일 내용
-     */
-    private String formatTodoContent(Todo todo) {
-        return todo.getContent();
+    private List<List<Long>> splitListIntoBatches(List<Long> userIds, int batchSize) {
+        return new ArrayList<>(userIds.stream()
+                .collect(Collectors.groupingBy(i -> i / batchSize))
+                .values());
     }
 }
