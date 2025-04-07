408d7f32c36ca97455de2f1aaeec1205215cb98c
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-04-07 19:33:12 +0900

    [feat] : 날짜 목록을 오름차순 정렬하여 반환한다 (#78)

--- src/main/java/server/poptato/todo/application/response/HistoryCalendarListResponseDto.java
+++ src/main/java/server/poptato/todo/application/response/HistoryCalendarListResponseDto.java
@@ -1,9 +1,9 @@
 package server.poptato.todo.application.response;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public record HistoryCalendarListResponseDto(
         List<HistoryCalendarResponseDto> historyCalendarList
@@ -12,6 +12,7 @@ public record HistoryCalendarListResponseDto(
     public static HistoryCalendarListResponseDto from(Map<LocalDate, Integer> dates) {
         List<HistoryCalendarResponseDto> historyCalendarList = dates.entrySet().stream()
                 .map(entry -> HistoryCalendarResponseDto.of(entry.getKey(), entry.getValue()))
+                .sorted(Comparator.comparing(HistoryCalendarResponseDto::localDate))
                 .toList();
 
         return new HistoryCalendarListResponseDto(historyCalendarList);
