94147d41c731ab329666a3fc0679617dc5269809
bbbang105 <2018111366@dgu.ac.kr>
2025-04-18 00:24:15 +0900

    #85 [remove] : 불필요한 리졸버를 제거한다

--- src/main/java/server/poptato/global/config/WebConfig.java
+++ src/main/java/server/poptato/global/config/WebConfig.java
@@ -2,15 +2,8 @@ package server.poptato.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import server.poptato.global.convertor.StringToMobileTypeConvertor;
-import server.poptato.global.resolver.AppVersionArgumentResolver;
-import server.poptato.global.resolver.MobileTypeArgumentResolver;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,16 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true)
                 .maxAge(3000);
-    }
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToMobileTypeConvertor());
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new MobileTypeArgumentResolver());
-        resolvers.add(new AppVersionArgumentResolver());
     }
 }
