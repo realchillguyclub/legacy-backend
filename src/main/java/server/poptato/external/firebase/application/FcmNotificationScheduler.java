c0a19e6fe2b932b053d0c80529aa09cc37f8dc35
bbbang105 <2018111366@dgu.ac.kr>
2025-04-18 21:49:34 +0900

    #89 [feat] : 하루 시작 & 일과 정리 푸쉬 알림 스케줄러를 추가한다

--- src/main/java/server/poptato/external/firebase/application/FcmNotificationScheduler.java
+++ src/main/java/server/poptato/external/firebase/application/FcmNotificationScheduler.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class FcmNotificationScheduler {
-    private final FcmNotificationService fcmNotificationService;
+    private final FcmNotificationBatchService fcmNotificationBatchService;
     private final FcmTokenService fcmTokenService;
 
     /**
      * 비활성 FCM 토큰을 삭제한다.
+     * - 기준: 최근 1개월간 사용되지 않은 토큰
      */
     @Scheduled(cron = "${scheduling.fcmCleanupCron}")
     public void deleteOldFcmTokens() {
@@ -20,11 +21,32 @@ public class FcmNotificationScheduler {
     }
 
     /**
-     * 마감일 알림을 전송한다.
+     * 하루 시작 푸쉬알림을 전송한다.
+     * - 대상: 푸쉬 알림을 허용한 전체 유저
+     */
+    @Scheduled(cron = "${scheduling.startNotificationCron}")
+    @Async
+    public void sendStartNotifications() {
+        fcmNotificationBatchService.sendStartNotifications();
+    }
+
+    /**
+     * 일과 정리 푸쉬알림을 전송한다.
+     * - 대상: 미완료 오늘 할 일이 존재하는 유저
+     */
+    @Scheduled(cron = "${scheduling.endOfDayNotificationCron}")
+    @Async
+    public void sendEndOfDayNotifications() {
+        fcmNotificationBatchService.sendEndOfDayNotifications();
+    }
+
+    /**
+     * 마감기한 푸쉬알림을 전송한다.
+     * - 대상: 오늘 마감인 할 일을 보유한 유저
      */
     @Scheduled(cron = "${scheduling.deadlineNotificationCron}")
     @Async
     public void sendDeadlineNotifications() {
-        fcmNotificationService.sendDeadlineNotifications();
+        fcmNotificationBatchService.sendDeadlineNotifications();
     }
 }
