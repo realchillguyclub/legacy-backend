0a99d8144e29a382fb03c15b9f28b5cb40defbcf
woong2e <tlswodnd12345@naver.com>
2025-05-22 13:54:40 +0900

    [feat] : 푸시알림 정책에 따라 메세지를 변경한다. (#103)

--- src/main/java/server/poptato/external/firebase/template/FcmNotificationTemplate.java
+++ src/main/java/server/poptato/external/firebase/template/FcmNotificationTemplate.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum FcmNotificationTemplate {
 
-    START_OF_DAY("일단 시작할 시간", "오늘 할 일을 확인하고 하루를 계획하세요!"),
-    END_OF_DAY("오늘 돌아보기", "계획한 할 일 중 완료한 것을 체크하세요!"),
+    START_OF_DAY("일단 시작할 시간", "할 일을 계획하고 하루를 시작하세요!"),
+    END_OF_DAY("하루 돌아보기", "완료한 일을 체크하세요"),
     DEADLINE("오늘 마감 예정인 할 일", "%s");
 
     private final String title;
