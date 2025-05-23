ff838517ca91a9713b444520d7568b20b6dbf547
woong2e <tlswodnd12345@naver.com>
2025-05-23 17:21:40 +0900

    [feat] : 시간 알림에 관련한 알림 템플릿을 추가한다. (#103)

--- src/main/java/server/poptato/external/firebase/template/FcmNotificationTemplate.java
+++ src/main/java/server/poptato/external/firebase/template/FcmNotificationTemplate.java
@@ -9,7 +9,8 @@ public enum FcmNotificationTemplate {
 
     START_OF_DAY("일단 시작할 시간", "할 일을 계획하고 하루를 시작하세요!"),
     END_OF_DAY("하루 돌아보기", "완료한 일을 체크하세요"),
-    DEADLINE("오늘 마감 예정인 할 일", "%s");
+    DEADLINE("오늘 마감 예정인 할 일", "%s"),
+    TIME_DEADLINE("할 일을 미리 준비하세요", "%s");
 
     private final String title;
     private final String body;
