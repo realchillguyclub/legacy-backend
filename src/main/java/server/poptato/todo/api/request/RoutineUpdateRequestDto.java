caa29646a79e6850ef887c3ac552598687a6ab28
bbbang105 <2018111366@dgu.ac.kr>
2025-05-25 15:37:55 +0900

    #111 [feat] : 루틴 요일 목록에 대한 유효성 검증을 추가한다

--- src/main/java/server/poptato/todo/api/request/RoutineUpdateRequestDto.java
+++ src/main/java/server/poptato/todo/api/request/RoutineUpdateRequestDto.java
@@ -1,8 +1,13 @@
 package server.poptato.todo.api.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public record RoutineUpdateRequestDto(
+        @NotNull
+        @Size(min = 1, message = "루틴 요일은 최소 1개 이상이어야 합니다.")
         List<String> routineDays
 ) {
 }
