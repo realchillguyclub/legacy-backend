5255da74b7b3df32a9b1da8d6606b4e5b51917ad
bbbang105 <2018111366@dgu.ac.kr>
2025-07-12 16:54:18 +0900

    #126 [feat] : 유저는 일단에게 의견을 보낼 수 있다

--- src/main/java/server/poptato/user/application/service/UserService.java
+++ src/main/java/server/poptato/user/application/service/UserService.java
@@ -1,11 +1,16 @@
 package server.poptato.user.application.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import server.poptato.auth.application.service.JwtService;
 import server.poptato.category.domain.repository.CategoryRepository;
+import server.poptato.user.api.request.UserCommentRequestDTO;
+import server.poptato.user.application.event.CreateUserCommentEvent;
+import server.poptato.user.domain.entity.Comment;
 import server.poptato.user.domain.entity.DeleteReason;
+import server.poptato.user.domain.repository.CommentRepository;
 import server.poptato.user.domain.repository.DeleteReasonRepository;
 import server.poptato.user.domain.value.Reason;
 import server.poptato.user.api.request.UserDeleteRequestDTO;
@@ -17,17 +22,18 @@ import server.poptato.user.validator.UserValidator;
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
     private final JwtService jwtService;
     private final UserValidator userValidator;
+    private final ApplicationEventPublisher eventPublisher;
+    private final UserRepository userRepository;
     private final DeleteReasonRepository deleteReasonRepository;
     private final CategoryRepository categoryRepository;
+    private final CommentRepository commentRepository;
 
     /**
-     * 사용자 탈퇴 처리
+     * 사용자 탈퇴 처리 메서드.
      *
      * 주어진 사용자 ID를 기반으로 탈퇴 요청을 처리합니다.
      * 탈퇴 이유를 저장하고, 관련 데이터(할 일, 모바일 정보)를 삭제한 뒤 사용자를 삭제합니다.
@@ -35,6 +41,7 @@ public class UserService {
      * @param userId 사용자 ID
      * @param userDeleteRequestDTO 탈퇴 요청 데이터
      */
+    @Transactional
     public void deleteUser(Long userId, UserDeleteRequestDTO userDeleteRequestDTO) {
         User user = userValidator.checkIsExistAndReturnUser(userId);
         saveDeleteReasons(userId, userDeleteRequestDTO.reasons(), userDeleteRequestDTO.userInputReason());
@@ -43,29 +50,6 @@ public class UserService {
         jwtService.deleteRefreshToken(String.valueOf(userId));
     }
 
-    /**
-     * 사용자 정보 조회
-     *
-     * 주어진 사용자 ID를 기반으로 사용자의 이름, 이메일, 이미지 URL 정보를 반환합니다.
-     *
-     * @param userId 사용자 ID
-     * @return 사용자 정보 DTO
-     */
-    @Transactional(readOnly = true)
-    public UserInfoResponseDto getUserInfo(Long userId) {
-        User user = userValidator.checkIsExistAndReturnUser(userId);
-        return UserInfoResponseDto.of(user);
-    }
-
-    /**
-     * 탈퇴 이유 저장
-     *
-     * 사용자가 선택한 탈퇴 이유와 직접 입력한 탈퇴 이유를 저장합니다.
-     *
-     * @param userId 사용자 ID
-     * @param reasons 선택된 탈퇴 이유 목록
-     * @param userInputReason 직접 입력한 탈퇴 이유
-     */
     private void saveDeleteReasons(Long userId, List<Reason> reasons, String userInputReason) {
         if (reasons != null && !reasons.isEmpty()) {
             reasons.forEach(reason -> {
@@ -83,5 +67,33 @@ public class UserService {
                     .build();
             deleteReasonRepository.save(deleteReason);
         }
+    }
+
+    /**
+     * 사용자 정보 조회 메서드.
+     *
+     * 주어진 사용자 ID를 기반으로 사용자의 이름, 이메일, 이미지 URL 정보를 반환합니다.
+     *
+     * @param userId 사용자 ID
+     * @return 사용자 정보 DTO
+     */
+    @Transactional(readOnly = true)
+    public UserInfoResponseDto getUserInfo(Long userId) {
+        User user = userValidator.checkIsExistAndReturnUser(userId);
+        return UserInfoResponseDto.of(user);
+    }
+
+    /**
+     * 사용자 의견 생성 메서드.
+     *
+     * @param userId 사용자 ID
+     * @param requestDTO 의견 정보
+     */
+    @Transactional
+    public void createAndSendUserComment(Long userId, UserCommentRequestDTO requestDTO) {
+        User user = userValidator.checkIsExistAndReturnUser(userId);
+        Comment comment = commentRepository.save(requestDTO.toEntity(user.getId()));
+
+        eventPublisher.publishEvent(CreateUserCommentEvent.from(comment, user.getName()));
     }
 }
