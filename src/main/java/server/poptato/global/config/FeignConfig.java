376492f2f1192d46ccdcfc9d64035fb18b52d7b8
bbbang105 <2018111366@dgu.ac.kr>
2025-07-13 14:31:22 +0900

    #126 [feat] : 노션 관련 FeignClient 설정을 추가한다

--- src/main/java/server/poptato/global/config/FeignConfig.java
+++ src/main/java/server/poptato/global/config/FeignConfig.java
@@ -1,9 +1,25 @@
 package server.poptato.global.config;
 
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(basePackages = "server.poptato.external.oauth")
+@EnableFeignClients(basePackages = "server.poptato.external")
 public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor notionRequestInterceptor(
+            @Value("${notion.secret-key}") String secretKey) {
+
+        return requestTemplate -> {
+            if (requestTemplate.feignTarget().name().startsWith("notion")) {
+                requestTemplate.header("Authorization", "Bearer " + secretKey);
+                requestTemplate.header("Notion-Version", "2022-06-28");
+                requestTemplate.header("Content-Type", "application/json");
+            }
+        };
+    }
 }
