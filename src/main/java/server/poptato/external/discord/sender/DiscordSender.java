727a4a6b0793184dc3d7536efb129dda01917cae
bbbang105 <2018111366@dgu.ac.kr>
2025-08-25 21:20:42 +0900

    ILLDAN-159 [refactor] : Properties 주입 방식으로 변경하여, 불필요한 인터페이스를 제거한다

--- src/main/java/server/poptato/external/discord/sender/DiscordSender.java
+++ src/main/java/server/poptato/external/discord/sender/DiscordSender.java
@@ -3,12 +3,10 @@ package server.poptato.external.discord.sender;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
-
 import lombok.RequiredArgsConstructor;
-import server.poptato.external.discord.client.DiscordCreateUserCommentWebhookClient;
-import server.poptato.external.discord.client.DiscordCreateUserWebhookClient;
-import server.poptato.external.discord.client.DiscordDailyStatsWebhookClient;
-import server.poptato.external.discord.client.DiscordDeleteUserWebhookClient;
+
+import server.poptato.external.discord.client.DiscordWebhookClient; // 변경됨
+import server.poptato.external.discord.config.DiscordWebhookProperties; // 추가됨
 import server.poptato.external.discord.dto.DailyStats;
 import server.poptato.external.discord.dto.DiscordMessage;
 import server.poptato.external.discord.formatter.DiscordMessageFormatter;
@@ -16,48 +14,44 @@ import server.poptato.user.application.event.CreateUserCommentEvent;
 import server.poptato.user.application.event.CreateUserEvent;
 import server.poptato.user.application.event.DeleteUserEvent;
 
+import java.net.URI;
+
 @Component
 @RequiredArgsConstructor
 public class DiscordSender {
 
-    private final DiscordCreateUserCommentWebhookClient discordCreateUserCommentWebhookClient;
-    private final DiscordCreateUserWebhookClient discordCreateUserWebhookClient;
-    private final DiscordDeleteUserWebhookClient discordDeleteUserWebhookClient;
-	private final DiscordDailyStatsWebhookClient discordDailyStatsWebhookClient;
+	private final DiscordWebhookClient discordWebhookClient;
+	private final DiscordWebhookProperties webhookProperties;
 
-	@Retryable(
-            retryFor = { Exception.class },
-            backoff = @Backoff(delay = 2000)
-    )
-    public void sendCreateUserCommentMessage(CreateUserCommentEvent event) {
-        String message = DiscordMessageFormatter.formatCreateUserCommentMessage(event);
-        discordCreateUserCommentWebhookClient.sendMessage(DiscordMessage.of(message));
-    }
+	// 유저 의견 전송 시
+	@Retryable(retryFor = {Exception.class}, backoff = @Backoff(delay = 2000))
+	public void sendCreateUserCommentMessage(CreateUserCommentEvent event) {
+		String message = DiscordMessageFormatter.formatCreateUserCommentMessage(event);
+		URI uri = URI.create(webhookProperties.getCreateUserCommentWebhookUrl());
+		discordWebhookClient.sendMessage(uri, DiscordMessage.of(message));
+	}
 
-    @Retryable(
-            retryFor = { Exception.class },
-            backoff = @Backoff(delay = 2000)
-    )
-    public void sendCreateUserMessage(CreateUserEvent event) {
-        String message = DiscordMessageFormatter.formatCreateUserMessage(event);
-        discordCreateUserWebhookClient.sendMessage(DiscordMessage.of(message));
-    }
+	// 유저 가입 시
+	@Retryable(retryFor = {Exception.class}, backoff = @Backoff(delay = 2000))
+	public void sendCreateUserMessage(CreateUserEvent event) {
+		String message = DiscordMessageFormatter.formatCreateUserMessage(event);
+		URI uri = URI.create(webhookProperties.getCreateUserWebhookUrl());
+		discordWebhookClient.sendMessage(uri, DiscordMessage.of(message));
+	}
 
-    @Retryable(
-            retryFor = { Exception.class },
-            backoff = @Backoff(delay = 2000)
-    )
-    public void sendDeleteUserMessage(DeleteUserEvent event) {
-        String message = DiscordMessageFormatter.formatDeleteUserMessage(event);
-        discordDeleteUserWebhookClient.sendMessage(DiscordMessage.of(message));
-    }
+	// 유저 탈퇴 시
+	@Retryable(retryFor = {Exception.class}, backoff = @Backoff(delay = 2000))
+	public void sendDeleteUserMessage(DeleteUserEvent event) {
+		String message = DiscordMessageFormatter.formatDeleteUserMessage(event);
+		URI uri = URI.create(webhookProperties.getDeleteUserWebhookUrl());
+		discordWebhookClient.sendMessage(uri, DiscordMessage.of(message));
+	}
 
-	@Retryable(
-		retryFor = { Exception.class },
-		backoff = @Backoff(delay = 2000)
-	)
+	// 일일 통계 집계
+	@Retryable(retryFor = {Exception.class}, backoff = @Backoff(delay = 2000))
 	public void sendDailyStatsMessage(DailyStats stats) {
 		String message = DiscordMessageFormatter.formatDailyStatsMessage(stats);
-		discordDailyStatsWebhookClient.sendMessage(DiscordMessage.of(message));
+		URI uri = URI.create(webhookProperties.getDailyStatsWebhookUrl());
+		discordWebhookClient.sendMessage(uri, DiscordMessage.of(message));
 	}
 }
